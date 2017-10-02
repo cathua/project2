c2358385515395c6fd9cd11cbb7f28008869a970
cat <catherine.hua@capitalone.com>
2017-10-02 12:12:31 -0400

    setting up routes, testing user creation

--- models/userMeetup.js
+++ models/userMeetup.js
@@ -2,7 +2,6 @@ module.exports = function(sequelize, DataTypes) {
     var userMeetup = sequelize.define('userMeetup', {
         user_id: DataTypes.INTEGER,
         meetup_id: DataTypes.INTEGER
-
     },
   {
     underscored: true
