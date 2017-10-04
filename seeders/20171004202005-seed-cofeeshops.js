b3030675bfe1a80ff7b9276492bde9fba41a00f3
Mingquan Ma <mingquan.ma@capitalone.com>
2017-10-04 18:57:37 -0400

    added 2 coffeeshops

---
+++ seeders/20171004202005-seed-cofeeshops.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('coffeeshops', [{
+        name: "Le Pain Quotidien",
+        address: "1961 Chain Bridge Rd",
+        city: "Tysons Corner",
+        state: "VA",
+        latitude: "38.9159306719076",
+        longitude: "-77.2230201777054"
+        rating: 3,
+        price: "$$",
+        createdAt: new Date(),
+        updatedAt: new Date()
+        },
+        {
+          name: "Peet's Coffee",
+          address: "8150 Leesburg Pike",
+          city: "Vienna",
+          state: "VA",
+          latitude: "38.9164",
+          longitude: "-77.22674"
+          rating: 4.5,
+          price: "$",
+          createdAt: new Date(),
+          updatedAt: new Date()
+          }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
