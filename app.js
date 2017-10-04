d56408fe1076cca0787fe120b3f7899bce0768c4
cat <catherine.hua@capitalone.com>
2017-10-04 09:27:21 -0400

    minor changes to app.js

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var meetups = require('./routes/meetups');
-//temporary! please delete later.
-var users = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,8 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/meetups', meetups);
-//temporary! please delete later.
-app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
