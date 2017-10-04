83b7b24d316af8b892e527eb8bafecf739f33abd
Mingquan Ma <mingquan.ma@capitalone.com>
2017-10-04 10:00:59 -0400

    made ajax call to get token, but now the login page doesn't redirect

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,18 @@ $(function() {
     .done(results => {
       localStorage.setItem('token', results.token);
       console.log('set token');
+      $.ajax({
+        method: "GET",
+        url: 'http://localhost:3000/users/api',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        },
+        json: true
+      })
+      .done(user => {
+        console.log(user);
+      })
+
     })
     .fail(err => {
       console.log('error');
