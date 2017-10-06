1dbb5fa70bc7998ab049b16055f05cee19ab7dfd
cat <catherine.hua@capitalone.com>
2017-10-06 02:07:49 -0400

    wrote 3 passing tests. refactored index.js to do so.

--- test/users_test.js
+++ test/users_test.js
@@ -2,3 +2,29 @@ var should    = require("chai").should(),
     expect    = require("chai").expect,
     supertest = require("supertest"),
     api       = supertest("http://localhost:3000");
+
+describe('GET /', function() {
+  it('should return a 200 response', function(done) {
+    api.get('/')
+    .set('accept', 'application/json')
+    .expect(200, done);
+  });
+});
+
+describe('GET authentication errors', function() {
+  it('accessing login error page directly returns 200 response', function(done) {
+    api.get('/index_login_error')
+    .set('accept', 'application/json')
+    .expect(200, done);
+  });
+
+  it('accessing signup error page directly returns 200 response', function(done) {
+    api.get('/index_signup_error')
+    .set('accept', 'application/json')
+    .expect(200, done);
+  });
+
+  // it('accessing through incorrect login returns 200 response', function(done) {
+  //   api.post
+  // })
+})
