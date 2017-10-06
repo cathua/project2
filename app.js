3849117dfda8fbe94b31de48d4d476a1b2c78af2
Mingquan Ma <mingquan.ma@capitalone.com>
2017-10-06 11:56:32 -0400

    added sequelize-cli to local files

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
     const uid = req.session.user.id;
 
     db.user.findById(uid, {
-      attributes: ['id', 'first_name', 'last_name', 'display_name']
+      attributes: ['id', 'f_name', 'l_name', 'username']
     })
       .then(user => {
         res.locals.user = user;
